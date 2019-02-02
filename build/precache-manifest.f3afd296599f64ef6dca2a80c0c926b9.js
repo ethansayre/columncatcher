48df7335a3291fad5852fe842cb51222998126cb
Ethan Sayre <ethansayre@Ethans-MacBook-Pro-3.local>
2019-02-01 22:23:39 -0600

    Built Chrome files.

---
+++ build/precache-manifest.f3afd296599f64ef6dca2a80c0c926b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99fab6dbb7999251a491",
+    "url": "/static/css/main.41f4b150.chunk.css"
+  },
+  {
+    "revision": "99fab6dbb7999251a491",
+    "url": "/static/js/main.99fab6db.chunk.js"
+  },
+  {
+    "revision": "39e19845782c70437ff5",
+    "url": "/static/css/1.11e86c6b.chunk.css"
+  },
+  {
+    "revision": "39e19845782c70437ff5",
+    "url": "/static/js/1.39e19845.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8286678a20684a456f8c6efdbeea0075",
+    "url": "/index.html"
+  }
+];
